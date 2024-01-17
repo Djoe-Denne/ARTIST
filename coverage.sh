d8911eca13b79f8c629b1fdfd15d368423317194
djoe denne <djoe.denne@gmail.com>
2024-01-17 16:38:03 +0100

    deactivate coverage ... again 'cause i'm a f******* looser

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./out/build/linux-debug/Cenpy/tests/CMakeFiles/cenpy-common_tests.dir/src
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+lcov --remove coverage.info '*conan*' --output-file coverage.info
+lcov --remove coverage.info '*test*' --output-file coverage.info
+lcov --list coverage.info 
+genhtml coverage.info --output-directory out
